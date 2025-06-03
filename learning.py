1525cd3ab397c27fe5c99b08beeadbbb3ad77fee
hevalo <eystreem2013@gmail.com>
2025-06-03 16:20:40 +0200

    edit

--- learning.py
+++ learning.py
@@ -2,6 +2,6 @@
 # - is minus
 # * is times
 # / is divided with float
-# // is divided without float hevalo salako
+# // is divided without float hevalo salako salko hevalo
 # ** is up
 print("this is basic math")
